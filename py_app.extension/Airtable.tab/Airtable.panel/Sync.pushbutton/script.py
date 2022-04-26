cdbef2d54f5367d29345faf5b2348ffcb9e4e781
jandersen <andersen.ja@gmail.com>
2022-04-26 16:43:43 -0400

    adds code for airtable sync

--- py_app.extension/Airtable.tab/Airtable.panel/Sync.pushbutton/script.py
+++ py_app.extension/Airtable.tab/Airtable.panel/Sync.pushbutton/script.py
@@ -10,12 +10,24 @@ pyRevit: repository at https://github.com/eirannejad/pyRevit
 from pyrevit import DB
 import rpw
 
+#general
+import requests
+
 revitRoomCollector = rpw.db.Collector(of_category=DB.BuiltInCategory.OST_Rooms, is_not_type=True)
 
 def roomInfo(collector):
     roomInfo = []
     for e in collector:
-        roomInfo.append({'name': e.Parameter[DB.BuiltInParameter.ROOM_NAME].AsString(), 'number': e.Number})
+        roomInfo.append({
+            'Name': e.Parameter[DB.BuiltInParameter.ROOM_NAME].AsString(),\
+             'Number': int(e.Number)})
     return roomInfo
 
-print(roomInfo(revitRoomCollector))
+airtableData = roomInfo(revitRoomCollector)
+
+url = "https://v1.nocodeapi.com/jrandersen/airtable/NBknQBAnYLitRlTH?tableName=rooms"
+params = {}
+data = airtableData
+r = requests.post(url = url, params = params, json = data)
+result = r.json()
+print(result)
