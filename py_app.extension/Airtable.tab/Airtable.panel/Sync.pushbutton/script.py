508272f425f0a9ab3d1d5db3407fe7d851fb58b3
jandersen <andersen.ja@gmail.com>
2022-04-26 15:03:57 -0400

    adds collector, and definition to parse rooms info

--- py_app.extension/Airtable.tab/Airtable.panel/Sync.pushbutton/script.py
+++ py_app.extension/Airtable.tab/Airtable.panel/Sync.pushbutton/script.py
@@ -1,4 +1,21 @@
-# base extension
+""" Revit to Airtable Tutorial
+Jason Andersen
+----------------------------------
+PyRevit Notice:
+Copyright (c) 2014-2022 Ehsan Iran-Nejad
+pyRevit: repository at https://github.com/eirannejad/pyRevit
+"""
 
-param = "Hello World"
-print(param)
+# pyRevit
+from pyrevit import DB
+import rpw
+
+revitRoomCollector = rpw.db.Collector(of_category=DB.BuiltInCategory.OST_Rooms, is_not_type=True)
+
+def roomInfo(collector):
+    roomInfo = []
+    for e in collector:
+        roomInfo.append({'name': e.Parameter[DB.BuiltInParameter.ROOM_NAME].AsString(), 'number': e.Number})
+    return roomInfo
+
+print(roomInfo(revitRoomCollector))
