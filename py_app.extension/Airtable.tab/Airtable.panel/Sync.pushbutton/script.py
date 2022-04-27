340338807086bee8b1ab0956e4b6e81d0751a50c
jandersen <andersen.ja@gmail.com>
2022-04-27 10:40:27 -0400

    update revit export gif

--- py_app.extension/Airtable.tab/Airtable.panel/Sync.pushbutton/script.py
+++ py_app.extension/Airtable.tab/Airtable.panel/Sync.pushbutton/script.py
@@ -7,27 +7,33 @@ pyRevit: repository at https://github.com/eirannejad/pyRevit
 """
 
 # pyRevit
-from pyrevit import DB
+from pyrevit import DB, forms
 import rpw
 
 #general
 import requests
 
-revitRoomCollector = rpw.db.Collector(of_category=DB.BuiltInCategory.OST_Rooms, is_not_type=True)
-
+# create definition to parse room collectors
 def roomInfo(collector):
-    roomInfo = []
+    roomInfo = []   
     for e in collector:
         roomInfo.append({
             'Name': e.Parameter[DB.BuiltInParameter.ROOM_NAME].AsString(),\
              'Number': int(e.Number)})
     return roomInfo
 
-airtableData = roomInfo(revitRoomCollector)
+# use pyrevit forms to show progress.
+with forms.ProgressBar(title='Exporting Workorders to Aitable base', indeterminate=True):
+    # make a list of rooms
+    revitRoomCollector = rpw.db.Collector(of_category=DB.BuiltInCategory.OST_Rooms, is_not_type=True)
 
-url = "https://v1.nocodeapi.com/jrandersen/airtable/NBknQBAnYLitRlTH?tableName=rooms"
-params = {}
-data = airtableData
-r = requests.post(url = url, params = params, json = data)
-result = r.json()
-print(result)
+    # instantiate def, get room info
+    airtableData = roomInfo(revitRoomCollector)
+
+    # do a basic post request to airtable through nocode api
+    url = "https://v1.nocodeapi.com/jrandersen/airtable/NBknQBAnYLitRlTH?tableName=rooms"
+    params = {}
+    data = airtableData
+    r = requests.post(url = url, params = params, json = data)
+    result = r.json()
+    print(result)
